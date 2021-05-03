3402483d17a6123ff384cb2309a96942eab9ab23
Vituz <vtbattaglia@gmail.com>
2021-05-03 19:47:03 +0200

    Complete dati section

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// // Nome passeggero
+// Codice treno (numero casuale tra 90000 e 100000 escluso)
+// Numero carrozza
+// Prezzo calcolato
+// Categoria selezionata dall'utente
+// Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", 
+// se clicchiamo su annulla dobbiamo ripulire il form.
